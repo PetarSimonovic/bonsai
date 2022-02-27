b0abb013e2d8c817d889bad85e0632b368d0228f
PetarSimonovic <petar@simonovic.com>
2022-02-27 10:52:52 +0000

    hill class added

---
+++ hill.py
@@ -0,0 +1,20 @@
+
+class Hill:
+    
+    def __init__(self, display, width, height):
+        self.display = display
+        self.width = width
+        self.height = height
+    
+    
+    
+    def draw_hill(self, season):
+         if season == "spring": 
+            self.display.set_pen(126, 200, 80)
+         elif season == "summer" or season == "autumn":
+            self.display.set_pen(126, 200, 100)
+         elif season == "winter":
+            self.display.set_pen(235, 235, 235)
+
+         self.display.circle(128, 360, 185) ## x, y, radius
+
