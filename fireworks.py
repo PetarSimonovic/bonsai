92bca189a11418beac4e1a44feab0f89a815e981
PetarSimonovic <petar@simonovic.com>
2022-02-27 16:58:41 +0000

    firework blast radius is random; sparks fizzle

--- fireworks.py
+++ fireworks.py
@@ -11,6 +11,7 @@ class Fireworks:
         self.height = height
         self.blast_radius = 0
         self.blast_limit = 0
+        self.fizzle = 0
         self.sparks = []
         
     def launch(self):
@@ -24,8 +25,10 @@ class Fireworks:
     def create_fireworks(self):
         self.create_blast_centre()
         self.blast_radius = 0
-        self.blast_limit = random.randint(15, 30)
+        self.blast_limit = random.randint(5, 30)
         self.create_sparks()
+        # 2 is a magic number radius value in Spark class
+        self.fizzle = 2/self.blast_limit
 
     def create_coordinate(self, range):
         return random.randint(0, range)
@@ -40,12 +43,12 @@ class Fireworks:
                 sparks.add_sparks(self.sparks, spark, self.x, self.y)
        
     def fly_sparks(self):
-        print("sparks fly")
         for spark in self.sparks:
             spark.x = sparks.get_x(self.sparks.index(spark), self.blast_radius, spark.x)
             spark.y = sparks.get_y(self.sparks.index(spark), self.blast_radius, spark.y)
             self.display.set_pen(spark.colour[0], spark.colour[1], spark.colour[2])
             self.display.circle(int(spark.x), int(spark.y), int(spark.r))
+            spark.r -= self.fizzle
         self.blast_radius += 1
         
 
