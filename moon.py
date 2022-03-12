99b49fde5f083979847edda256a195fe2765de94
PetarSimonovic <petar@simonovic.com>
2022-03-12 14:22:52 +0000

    moon has craters

--- moon.py
+++ moon.py
@@ -6,7 +6,20 @@ class Moon:
         self.display = display
         self.width = width
         self.height = height
+        self.radius = 20
         
     def rise(self):
-        self.display.set_pen(200, 200, 200)
-        self.display.circle(int(self.width/2), int(50), int(20))
+        x = self.width/2
+        y = 50
+        self.display.set_pen(180, 180, 180)
+        self.display.circle(int(x), int(y), int(self.radius))
+        self.display.set_pen(220, 220, 220)
+        self.display.circle(int(x - 5), int(y), int(self.radius))
+        self.display.set_pen(210, 210, 210)
+        self.display.circle(int(x - 10), int(y - 10), int(self.radius/3.5))
+        self.display.set_pen(210, 210, 210)
+        self.display.circle(int(x - 7), int(y + 8), int(self.radius/4))
+        self.display.circle(int(x - 9), int(y + 5), int(self.radius/3))
+        self.display.set_pen(208, 208, 208)
+        self.display.circle(int(x + 5), int(y - 6), int(self.radius/5))
+        self.display.circle(int(x + 8), int(y + 3), int(self.radius/4))
